39526af8596e013a0c453cf2cdc35a1f82026233
Jamoxidase <128327009+Jamoxidase@users.noreply.github.com>
2024-04-24 14:10:51 -0700

    Update script.js

--- script.js
+++ script.js
@@ -50,6 +50,14 @@ for (let i = 0; i < numberOfBalls; i++) {
         });
     }
 
+    function getCenter(element) {
+    const rect = element.getBoundingClientRect();
+    return {
+        x: rect.left + rect.width / 2,
+        y: rect.top + rect.height / 2
+    };
+}
+
 
 function launchBalls() {
         balls.forEach(ball => {
